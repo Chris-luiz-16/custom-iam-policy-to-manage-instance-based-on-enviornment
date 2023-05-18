454486b9950f3b4adebdb35cb7106c94a2418127
Chris-luiz-16 <128575317+Chris-luiz-16@users.noreply.github.com>
2023-05-18 22:56:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA5YKGNPEXMXI3AEUM"
-  secret_key = "kMNKrzW97p9gbqU180ruCM0PykHAmzjtXLo7wkro"
+  access_key = "<Your Access_key>"
+  secret_key = "<Your secret Key>"
 }
